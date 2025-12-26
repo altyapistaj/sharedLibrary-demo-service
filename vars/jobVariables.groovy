5a86a31ca580f3e8ff176a3b88137cb694753473
altyapistaj <altyapi_staj@proton.me>
2025-12-26 15:34:55 +0300

    initial commit

--- vars/jobVariables.groovy
+++ vars/jobVariables.groovy
@@ -1,16 +1,4 @@
 def call(String JOB_NAME){
-    '''
-    return [
-            projectName : 'demo-service-test',
-            jobName : "",
-            pom : 'pom.xml',
-
-            branch : "library-dependency",
-            customWorkspace : "workspace/" + JOB_NAME + "/",
-            gitPathName : "altyapistaj",
-            gitAdressAndName : "demo-service"
-    ]
-    '''
     return [
             projectName : 'demo-service-test',
             jobName : JOB_NAME.tokenize('/')[1],
@@ -19,7 +7,7 @@ def call(String JOB_NAME){
 
 
             customWorkspace : "workspace/" + JOB_NAME + "/",
-            gitAdressAndName : "demo-service"
+            gitAdressAndName : JOB_NAME.tokenize('/')[1]
     ]
 
 }
